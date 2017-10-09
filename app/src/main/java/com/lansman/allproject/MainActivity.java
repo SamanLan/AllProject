d32fbef032baf683fe4084e18b4decc146e169f0
zixin <lanshenming@linghit.com>
2017-10-09 18:31:56 +0800

    2017-10-08 RxJava使用，线程调度等原理了解

--- app/src/main/java/com/lansman/allproject/MainActivity.java
+++ app/src/main/java/com/lansman/allproject/MainActivity.java
@@ -3,11 +3,69 @@ package com.lansman.allproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.internal.schedulers.NewThreadScheduler;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        deal();
+    }
+
+    private void deal() {
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<Object>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Object> e) throws Exception {
+                System.out.println("订阅在" + Thread.currentThread().getName());
+                e.onComplete();
+            }
+        }).subscribeOn(new NewThreadScheduler())
+                .map(new Function<Object, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Object o) throws Exception {
+                        return o.toString().length();
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Function<Integer, String>() {
+                    @Override
+                    public String apply(@NonNull Integer integer) throws Exception {
+                        return "第" + integer + "项";
+                    }
+                });
+        observable.subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String o) {
+                        System.out.println("观察在" + Thread.currentThread().getName());
+                        System.out.println(o);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("完成");
+                    }
+                });
+
     }
 }
